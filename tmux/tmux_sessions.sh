cec00c514ec132d412bdea680374d0be18380ece
jvl <joaquim.luis@starkdata.ai>
2024-12-18 11:41:51 +0000

    Add bind to reload config, comments and start script

---
+++ tmux/tmux_sessions.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# To chain tmux commands must escape semicolon, like ';', otherwise shell
+# interprets it has a shell command
+
+# 'send' is alias for 'send-keys'
+
+SESSION_NAME="platform"
+CHANGE_DIR="pfb"
+
+# check if session already exists
+if tmux has-session -t $SESSION_NAME 2>/dev/null; then
+    echo "Session $SESSION_NAME already exists, attaching to it."
+    tmux attach-session -t $SESSION_NAME 
+else
+    # start tmux in detached mode and rename it
+    tmux new -s $SESSION_NAME -d
+    tmux rename-window main ';' send $CHANGE_DIR C-m
+    # create new windows and rename
+    tmux new-window -t $SESSION_NAME ';' rename-window pytest ';' send $CHANGE_DIR C-m
+    tmux new-window -t $SESSION_NAME ';' rename-window shell ';' send $CHANGE_DIR C-m
+fi
+
+SESSION_NAME=audio
+if tmux has-session -t $SESSION_NAME 2>/dev/null; then
+    echo "Session $SESSION_NAME already exists, attaching to it."
+    tmux attach-session -t $SESSION_NAME 
+else
+    tmux new -s $SESSION_NAME ';' send-keys "yt" C-m
+fi

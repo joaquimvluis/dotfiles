95dda9451cd832f6d343bee9960c5cd9d2616d98
Joaquim Luis <jluis@alter-solutions.com>
2025-05-27 10:44:32 +0100

    wezterm: add config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,44 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- Disable all default keybindings assignments
+-- config.disable_default_key_bindings = true
+
+-- This is where you actually apply your config choices
+
+function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "Catppuccin Mocha"
+	else
+		return "Catppuccin Latte"
+	end
+end
+
+config.enable_tab_bar = false
+
+config.window_decorations = "RESIZE"
+
+config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+
+config.font = wezterm.font("RobotoMono Nerd Font")
+config.font_size = 19
+
+config.keys = {
+	-- vertical split (right)
+	{
+		key = "o",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "e",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+}
+
+-- and finally, return the configuration to wezterm
+return config
